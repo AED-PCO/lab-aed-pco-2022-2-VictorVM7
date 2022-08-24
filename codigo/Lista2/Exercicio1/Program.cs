d1ae7a7b2f8d8a5fb6457f74fd359d721f7b86a6
VictorVM7 <vicmagalhaes20@gmail.com>
2022-08-24 14:02:53 -0300

    Lista 2 Exercicio 1

--- codigo/Lista2/Exercicio1/Program.cs
+++ codigo/Lista2/Exercicio1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}! = {1}", numero, calculaFatorial(numero));
+        }
+
+        static int calculaFatorial(int num)
+        {
+            if(num == 2)
+                return 2;
+            else
+                return num * calculaFatorial(num - 1);
+        }
+    }
+}
+        
