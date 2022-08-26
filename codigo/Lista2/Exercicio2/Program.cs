388f6d3bcda0600015923cdc5f432d3f32e4c409
VictorVM7 <vicmagalhaes20@gmail.com>
2022-08-25 21:11:52 -0300

    Lista 2 Exercicio 4

---
+++ codigo/Lista2/Exercicio2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            int numeroA = int.Parse(Console.ReadLine());
+            int numeroB = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}*{1} = {2}", numeroA, numeroB, multiplica(numeroA, numeroB));
+        }
+
+        static int multiplica(int numA, int numB)
+        {
+            if(numB == 0)
+            {
+                numA = 0;
+                return numA;
+            }
+                
+            if(numB == 1)
+                return numA;
+            else
+                return numA + multiplica(numA, numB - 1);
+        }
+    }
+}
+     
---
+++ codigo/Lista2/Exercicio4/Program.cs
@@ -0,0 +1,23 @@
+﻿class program
+{
+    static int leLetra(string frase, char letra)
+    {
+        int cont = 0;
+        for (int i = 0; i < frase.Length; i++)
+        {
+            if(frase[i] == letra)
+                cont++;
+        }
+        return cont;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite um frase:");
+        string frase = Console.ReadLine();
+        Console.WriteLine("Digite um char a pesquisar:");
+        char letra = char.Parse(Console.ReadLine());
+
+        Console.WriteLine(leLetra(frase, letra));
+    }
+}
