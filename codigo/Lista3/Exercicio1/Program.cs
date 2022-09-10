3cfd30673d046e7fddfb95fbf5ac0631364c463e
VictorVM7 <vicmagalhaes20@gmail.com>
2022-09-10 16:08:17 -0300

    Lista 3 busca binária

--- codigo/Lista3/Exercicio1/Program.cs
+++ codigo/Lista3/Exercicio1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    
+        
+
+    static void preencheVetor(int[] vetor)
+    {
+        for(int i = 0; i < vetor.Length; i++)
+        {
+            Random numAleatorio = new Random();
+            vetor[i] = numAleatorio.Next(1,50);
+            Console.WriteLine(vetor[i]);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        preencheVetor(vetor)
+    }    
--- codigo/Lista3/Exercicio4/Program.cs
+++ codigo/Lista3/Exercicio4/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.CompilerServices;
+
+class program
+{
+    static string pesquisaVetor(int[] vetor, int ini, int mei, int fim, int num)
+    {
+        mei = (ini + fim)/2;
+
+        if(num == vetor[mei])
+            return $"Número {num} encontrado!";
+
+        else if(num > vetor[ini] && num < vetor[mei])
+        {
+            fim = mei - 1;
+            return pesquisaVetor(vetor, ini, mei, fim, num);
+        }
+
+        else if(num > vetor[mei] && num < vetor[fim])
+        {
+            ini = mei + 1;
+            return pesquisaVetor(vetor, ini, mei, fim, num);
+        }
+        
+        return "-1";
+    }
+
+    static void Main(string[] args)
+    {
+        int[] vetor = new int[8] {0, 1, 2, 3, 4, 5, 6, 7};
+        int numero = 6;
+
+        int inicio = 0;
+        int meio = 0;
+        int fim = vetor.Length - 1;
+        
+        Console.WriteLine(pesquisaVetor(vetor, inicio, meio, fim, numero));
+    }
+}
