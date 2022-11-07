afad3ba48cc133263ba04dcdce121cbeafdafe68
Victor Magalhães De Souza <1266914@sga.pucminas.br>
2022-11-07 20:40:16 -0300

    Lista 9 Incio Fila Inversa

--- codigo/Lista9/exercicio1/Program.cs
+++ codigo/Lista9/exercicio1/Program.cs
@@ -1,90 +1,96 @@
-﻿using System.Globalization;
+﻿using System;
 
-internal class Program
+namespace exercicio1
 {
-    //Imprime o vetor
-    static void imprimeVetor(int[] vet, int pos)
+    internal class Program
     {
-        Console.Write("Pilha: ");
-        for (int i = 0; i < pos; i++)
+        //Imprime o vetor
+        static void imprimeVetor(int[] vet, int pos)
         {
-            Console.Write(vet[i] + " ");
+            Console.Write("Fila: ");
+            for (int i = 0; i < pos; i++)
+            {
+                Console.Write(vet[i]);
+            }
+            Console.Write("\n");
         }
-        Console.Write("\n");
-    }
-
-    // Main
-    static void Main(string[] args)
-    {
-        int[] vet = new int[4];
-        int[] vetor = new int[4];
-        int pos = 0;
-
-        while (pos >= 0)
+        // Main
+        static void Main(string[] args)
         {
-            Console.Beep();
-            Console.WriteLine("Deseja inserir, retirar ou imprimir pilha\n[I] para inseri\n[R] para retirar\n[V] para Imprimir vetor");
-            string escolha = Console.ReadLine().ToLower();
+            int[] vet = new int[4];
+            int[] vetor = new int[vet.Length];
+            int[] filaInversa = new int[vetor.Length];
+            int pos = 0;
 
-            if (escolha.Equals("s"))
-                break;
+            while (pos >= 0)
+            {
+                Console.Beep(800, 200);
+                Console.WriteLine("Deseja inserir, retirar ou imprimir vetor\n[I] para inseri\n[R] para retirarz\n[V] para Imprimir vetor");
+                string escolha = Console.ReadLine().ToLower();
 
-            else if (escolha.Equals("i") && pos < vetor.Length)
-            {
-                vetor = insere(vet, pos);
-                pos++;
-            }
-            else if (escolha.Equals("r") && pos != 0)
-            {
-                retira(vetor, pos);
-                pos--;
-            }
-            else if (escolha.Equals("v"))
-            {
-                if (pos != 0)
-                    imprimeVetor(vetor, pos);
+                if (escolha.Equals("s"))
+                    break;
+
+                else if (escolha.Equals("i") && pos < vetor.Length)
+                {
+                    vetor = insere(vet, pos);
+                    pos++;
+                }
+                else if (escolha.Equals("r") && pos != 0)
+                {
+                    retira(vetor, pos);
+                    insereInverso(filaInversa, filaInversa.Length - pos);
+                    Console.WriteLine("Número na primeira posição retirado!");
+                    pos--;
+                }
+                else if (escolha.Equals("v"))
+                {
+                    if (pos != 0)
+                    {
+                        imprimeVetor(vetor, pos);
+                        imprimeVetor(filaInversa, filaInversa.Length);
+                    }
+                    else
+                        Console.WriteLine("Vetor vazio!");
+                }
+                else if (pos >= vetor.Length)
+                {
+                    Console.WriteLine("Seu vetor já está cheio! Você pode retirar ou imprimir!");
+                }
+                else if (pos == 0)
+                {
+                    Console.WriteLine("Seu vetor está vazio! Você pode inserir ou imprimir!");
+                }
                 else
-                    Console.WriteLine("Pilha vazia!");
-            }
-            else if (pos >= vetor.Length)
-            {
-                Console.WriteLine("Sua pilha está cheia! Você pode retirar ou imprimir!");
-            }
-            else if (pos == 0)
-            {
-                Console.WriteLine("Sua pilha está vazia! Você pode inserir ou imprimir!");
-            }
-            else
-            {
-                Console.WriteLine("Escolha não aplicada.");
-                break;
+                {
+                    Console.WriteLine("Escolha não aplicada.");
+                    break;
+                }
             }
         }
-    }
-
-    // Insere valores na pilha
-    static int[] insere(int[] vetor, int pos)
-    {
-        Console.WriteLine($"Preencha a pilha na posição {pos + 1}: ");
-        vetor[pos] = int.Parse(Console.ReadLine());
-        return vetor;
-    }
-
-    // Retira valores da pilha
-    static int[] retira(int[] vet, int pos)
-    {
-        string escolha;
-        for (int i = pos; i > 0; i--)
+        // Insere valores na fila
+        static int[] insere(int[] vetor, int pos)
         {
-            Console.WriteLine($"Retirar número na posição {pos}? [S] Sim e [N] Não");
-            escolha = Console.ReadLine().ToLower();
-            if (escolha.Equals('s'))
-            {
-                vet[i - 1] = vet[i];
-            }
-            else
-                break;
+            Console.Write($"Preencha a fila na posição {pos + 1}: ");
+            vetor[pos] = int.Parse(Console.ReadLine());
+            return vetor;
         }
-        return vet;
+        // Insere inverso na fila inversa *******************************
+        static int[] insereInverso(int[] vetor, int pos, int posInv)
+        {
+            vetor[vetor.Length - 1] = vetor[pos];
+            return vetor;
+        }
+        // Retira valores da fila
+        static int[] retira(int[] vet, int pos)
+        {
+            int aux = 0;
+            aux = vet[0];
+            for (int i = 0; i < pos - 1; i++)
+            {
+                vet[i] = vet[i + 1];
+            }
+            return vet;
+        }
     }
 }
