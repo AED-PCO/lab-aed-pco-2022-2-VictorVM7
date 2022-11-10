b62d1c68c22b134aa8619149d0f6a7d8e7b95e6d
VictorVM7 <vicmagalhaes20@gmail.com>
2022-11-10 19:36:38 -0300

    Atualizando

--- codigo/LIsta8/exercicio1/Program.cs
+++ codigo/LIsta8/exercicio1/Program.cs
@@ -4,74 +4,12 @@ namespace exercicio1
 {
     internal class Program
     {
-        //Imprime o vetor
-        static void imprimeVetor(int[] vet, int pos) {
-            Console.Write("Fila: ");
-            for (int i = 0; i < pos; i++) {
-                Console.Write(vet[i]);
-            }
-            Console.Write("\n");
-        }
-        // Main
-        static void Main(string[] args) {
-            int[] vet = new int[4];
-            int[] vetor = new int[4];
-            int pos = 0;
-           
-            while (pos >= 0) {
-                Console.WriteLine("Deseja inserir, retirar ou imprimir vetor\n[I] para inseri\n[R] para retirarz\n[V] para Imprimir vetor");
-                string escolha = Console.ReadLine().ToLower();
+		static void Main(string[] args){
 
-                if (escolha.Equals("s"))
-                    break;
+			15 -------- 25
+            7 --------- x
+            
 
-                else if (escolha.Equals("i") && pos < vetor.Length)
-                {
-                    vetor = insere(vet, pos);
-                    pos++;
-                }
-                else if (escolha.Equals("r") && pos != 0)
-                {
-                    retira(vetor, pos);
-                    Console.WriteLine("Número na primeira posição retirado!");
-                    pos--;
-                }
-                else if (escolha.Equals("v"))
-                {
-                    if (pos != 0)
-                        imprimeVetor(vetor, pos);
-                    else
-                        Console.WriteLine("Vetor vazio!");
-                }
-                else if (pos >= vetor.Length)
-                {
-                    Console.WriteLine("Seu vetor já está cheio! Você pode retirar ou imprimir!");
-                }
-                else if (pos == 0)
-                {
-                    Console.WriteLine("Seu vetor está vazio! Você pode inserir ou imprimir!");
-                }
-                else
-                {
-                    Console.WriteLine("Escolha não aplicada.");
-                    break;
-                }   
-            }
-        }
-        // Insere valores na fila
-        static int[] insere(int[] vetor, int pos) {
-            Console.Write($"Preencha a fila na posição {pos + 1}: ");
-            vetor[pos] = int.Parse(Console.ReadLine());
-            return vetor;
-        }
-        // Retira valores da fila
-        static int[] retira(int[] vet, int pos) {
-            int aux = 0;
-            aux = vet[0];
-            for (int i = 0; i < pos - 1; i++) {
-                vet[i] = vet[i + 1];
-            }
-            return vet;
-        }
+		}
     }
 }
---
+++ codigo/Lista7/exercicio2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+class programa
+{
+    // Lê o arquivo
+    static void Main(string[] args)
+    {
+        StreamReader docTxt = new StreamReader("C:\\Users\\victo\\source\\repos\\AED-PCO\\lab-aed-pco-2022-2-VictorVM7\\codigo\\Lista7\\exercicio1\\docTxt.txt");
+        Console.WriteLine("Enter a text:");
+        string userTxt = Console.ReadLine();
+        string txtDoc = docTxt.ReadLine();
+
+
+    }
+}
