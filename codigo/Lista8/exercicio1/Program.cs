f84b13baf15f5d6f3df9d9abb7164c3da33a5117
VictorVM7 <vicmagalhaes20@gmail.com>
2022-11-17 22:03:20 -0300

    Fila inversa e Pilha em ordem original

--- codigo/Lista8/exercicio1/Program.cs
+++ codigo/Lista8/exercicio1/Program.cs
@@ -4,31 +4,63 @@ namespace exercicio1
 {
     internal class Program
     {
+        //Imprime o vetor
+        static void imprimeVetor(int[] vet, int pos)
+        {
+            Console.Write("Fila: ");
+            for (int i = 0; i < pos; i++)
+            {
+                Console.Write(vet[i]);
+            }
+            Console.Write("\n");
+        }
+
+        // Main
         static void Main(string[] args)
         {
-            //Imprime o vetor
-            static void imprimeVetor(int[] vet, int pos)
+            int[] vet = new int[4];
+            int[] vetor = new int[4];
+            int pos = 0;
+
+            while (pos >= 0)
             {
-                Console.Write("Fila: ");
-                for (int i = 0; i < pos; i++)
+                Console.Beep();
+                Console.WriteLine("Deseja inserir, retirar ou imprimir vetor\n[I] para inseri\n[R] para retirarz\n[V] para Imprimir vetor");
+                string escolha = Console.ReadLine().ToLower();
+
+                if (escolha.Equals("s"))
+                    break;
+
+                else if (escolha.Equals("i") && pos < vetor.Length)
                 {
-                    Console.Write(vet[i]);
+                    vetor = insere(vet, pos);
+                    pos++;
                 }
-                Console.Write("\n");
-            }
-
-            // Main
-            static void Main(string[] args)
-            {
-                int[] vet = new int[4];
-                int[] vetor = new int[4];
-                int pos = 0;
-
-                while (pos >= 0)
+                else if (escolha.Equals("r") && pos != 0)
                 {
-                    Console.Beep();
-                    Console.WriteLine("Deseja inserir, retirar ou imprimir vetor\n[I] para inseri\n[R] para retirarz\n[V] para Imprimir vetor");
-                    string escolha = Console.ReadLine().ToLower();
+                    retira(vetor, pos);
+                    pos--;
+                }
+                else if (escolha.Equals("v"))
+                {
+                    if (pos != 0)
+                    {
+                        imprimeVetor(vetor, pos);
+                    }
+                    else Console.WriteLine("Pilha vazia!");
+                }
+                else if (pos >= vetor.Length)
+                {
+                    Console.WriteLine("Sua pilha está cheia! Você pode retirar ou imprimir!");
+                }
+                else if (pos == 0)
+                {
+                    Console.WriteLine("Sua pilha está vazia! Você pode inserir ou imprimir!");
+                }
+                else
+                {
+                    Console.WriteLine("Escolha não aplicada.");
+                    break;
                 }
             }
 
