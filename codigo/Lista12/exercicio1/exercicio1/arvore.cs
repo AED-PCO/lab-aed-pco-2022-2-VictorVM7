f8449012b05f459f0fd9ae9f6d882020ccfdab42
Victor Magalhães De Souza <1266914@sga.pucminas.br>
2022-12-12 20:37:16 -0300

    Arvore ABB

---
+++ codigo/Lista12/exercicio1/exercicio1/arvore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arvore
+{
+    class arvore
+    {
+        nodulo noduloPai = new nodulo();
+
+        public static void inserir(nodulo pai, int valor)
+        {
+            if(pai.valor < valor)
+            {
+                if(pai.noduloDir == null)
+                {
+                    nodulo noduloFilho = new nodulo(); 
+                    noduloFilho.valor = valor;
+                    noduloFilho.noduloPai = pai;
+                    pai.noduloDir = noduloFilho;
+                }
+                else
+                {
+                    inserir(pai.noduloDir, valor);
+                }
+            }
+        }
+    }
+
+    class nodulo
+    {
+        public nodulo noduloPai;
+        public nodulo noduloEsq;
+        public nodulo noduloDir;
+        public int valor;
+    }
+}
