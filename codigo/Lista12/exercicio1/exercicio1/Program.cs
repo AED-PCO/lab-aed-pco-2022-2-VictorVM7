61773056da94f63f9af17cbb19b3ceb36b5553c5
VictorVM7 <vicmagalhaes20@gmail.com>
2022-12-15 14:03:19 -0300

    Arvore, Inserir e Buscar

--- codigo/Lista12/exercicio1/exercicio1/Program.cs
+++ codigo/Lista12/exercicio1/exercicio1/Program.cs
@@ -1,2 +1,87 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using arvore;
+using funcoes;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        nodulo nodulo = new nodulo();
+
+        Boolean continuar = true;
+        int valorDigitado = 0;
+        int escolheFuncao;  // 1 - Insere | 2 - Busca | 3 - remove
+        string resposta;
+
+        while (continuar)
+        {
+            Console.WriteLine("--ARVORE--\nEscolha um opção digitando o número:\n[1] Inserir\n[2] Buscar\n[3] Remover");
+            escolheFuncao = int.Parse(Console.ReadLine());
+
+            switch (escolheFuncao)
+            {
+                case 1:
+                    chamaFuncaoInserir(valorDigitado, nodulo);
+                    break;
+
+                case 2:
+                    chamaFuncaoBusca(valorDigitado, nodulo);
+                    break;
+
+                case 3:
+                    chamaFuncaoBusca(valorDigitado, nodulo);
+                    break;
+
+                default:
+                    Console.WriteLine("Instrução inválida!");
+                    Console.Clear();
+                    break;
+            }
+
+            Console.WriteLine("Deseja continuar? [S] para continuar [N] para sair");
+            resposta = Console.ReadLine().ToLower();
+
+            if(resposta.Equals("n"))
+                continuar = false;
+            Console.Clear();
+        }
+    }
+
+    static void chamaFuncaoInserir(int valor, nodulo nodulo)
+    {
+        try
+        {
+            Console.WriteLine("Digite um valor a INSERIR na árvore: ");
+            valor = int.Parse(Console.ReadLine());
+            funcoes.funcoes.Inserir(nodulo, valor);
+        }
+        catch(Exception)
+        {
+            Console.WriteLine("Digite um valor válido!");
+        }        
+    }
+
+    static void chamaFuncaoBusca(int valor, nodulo nodulo)
+    {
+        Boolean numeroEncontrado;
+
+        Console.WriteLine("Digite um valor a BUSCAR na árvore: ");
+        valor = int.Parse(Console.ReadLine());
+
+        numeroEncontrado = funcoes.funcoes.Busca(nodulo, valor);
+
+        if (numeroEncontrado)
+        {
+            Console.WriteLine($"Número {valor} encontrado na árvore!");
+        }
+        else
+        {
+            Console.WriteLine($"Não foi possível encontrar o {valor} na árvore!\n");
+        }
+    }
+}
--- codigo/Lista12/exercicio1/exercicio1/arvore.cs
+++ codigo/Lista12/exercicio1/exercicio1/arvore.cs
@@ -6,29 +6,6 @@ using System.Threading.Tasks;
 
 namespace arvore
 {
-    class arvore
-    {
-        nodulo noduloPai = new nodulo();
-
-        public static void inserir(nodulo pai, int valor)
-        {
-            if(pai.valor < valor)
-            {
-                if(pai.noduloDir == null)
-                {
-                    nodulo noduloFilho = new nodulo(); 
-                    noduloFilho.valor = valor;
-                    noduloFilho.noduloPai = pai;
-                    pai.noduloDir = noduloFilho;
-                }
-                else
-                {
-                    inserir(pai.noduloDir, valor);
-                }
-            }
-        }
-    }
-
     class nodulo
     {
         public nodulo noduloPai;
