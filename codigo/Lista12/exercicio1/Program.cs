e9e4c07b38f0f872e4343eac0fc996f63cf8a5f6
VictorVM7 <vicmagalhaes20@gmail.com>
2022-12-15 18:37:56 -0300

    atualizanod

---
+++ codigo/Lista12/exercicio1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Arvore;
+
+namespace exercicio1
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+            Nodulo no = new ElementoArvore();
+            Console.WriteLine(no.elemento.valor);
+        }
+    }
+}
---
+++ codigo/Lista12/exercicio1/arvore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arvore
+{
+    public class Nodulo
+    {
+        public int valor = 10;
+    }
+
+    public class ElementoArvore
+    {
+        Nodulo nodulo = new Nodulo();
+
+    }
+}
+
+
+
