e89d8f2631b43a3f38c67732310184538e1d7147
VictorVM7 <vicmagalhaes20@gmail.com>
2022-12-15 22:55:18 -0300

    Finalizando

---
+++ codigo/Lista11/exercicio1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ainda em construção! Não consegui fazer esse Lab a tempo :(");
+        }
+    }
+}
--- codigo/Lista12/exercicio1/arvore.cs
+++ codigo/Lista12/exercicio1/arvore.cs
@@ -4,19 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Arvore
+namespace arvore
 {
-    public class Nodulo
+    class nodulo
     {
-        public int valor = 10;
-    }
-
-    public class ElementoArvore
-    {
-        Nodulo nodulo = new Nodulo();
-
+        public nodulo noduloPai;
+        public nodulo noduloEsq;
+        public nodulo noduloDir;
+        public int valor;
     }
 }
-
-
-
